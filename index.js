dc3689be64385811b648b68665d1c4a0e62227c7
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-03-16 19:04:17 +0100

    completo

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 function drawSet(ctx, S) {
-    ctx.beginPath()
-    ctx.arc(S.x, S.y, S.r, 0, 2 * Math.PI)
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.moveTo(S.x, S.y)
-    
-    if (S.r < 0.01) return
+    const draw_arc = false
+    const draw_tree = true
+    if (draw_arc) {
+        ctx.beginPath()
+        ctx.arc(S.x, S.y, S.r, 0, 2 * Math.PI)
+        ctx.stroke()
+    }    
+    if (S.r < 0.005) return
     for (const child of S.refine()) {
+        if (draw_tree) {
+            ctx.beginPath()
+            ctx.moveTo(S.x, S.y)
+            ctx.lineTo(child.x, child.y)
+            ctx.stroke()
+        }    
         drawSet(ctx, child)
     }
 }
 
 function draw(canvas) {
     const ctx = canvas.getContext('2d')
-    const S = new SteinerSet(0.2)
+    const S = new SteinerSet(0.55)
     ctx.translate(canvas.width/2, canvas.height/2)
-    const s = 0.4*Math.min(canvas.width, canvas.height)
+    const s = 0.2*Math.min(canvas.width, canvas.height)
     ctx.scale(s,s)
     ctx.lineWidth /= s
     drawSet(ctx, S)
